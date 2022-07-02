609fbff8aa080efc2991c31c6f3cb7a80943c9c9
EdnaK18 <reginakwanrx@gmail.com>
2022-07-02 17:03:13 -0400

    fixed bugs for front end rendering picked restaurant

--- client/components/NavBar1.jsx
+++ client/components/NavBar1.jsx
@@ -5,21 +5,21 @@ import { Link } from 'react-router-dom';
 export default function NavBar1() {
 
   //add clickHandlers to render either log in or sign up 
-  const LogIn = () => {
+  // const LogIn = () => {
 
-  }
+  // }
 
 
   return (
     <nav className='nav'>
       <Link to="/Landing" className='site-name'>Pick For Me</Link>
       <ul className='navbar'>
-        <li>
-          <button onClick={logIn}><p>Log In</p></button>
+        {/* <li>
+          <button onClick={()=>logIn}><p>Log In</p></button>
         </li>
         <li>
-          <button onClick={signUp}><p>Sign Up</p></button>
-        </li>
+          <button onClick={()=>signUp}><p>Sign Up</p></button>
+        </li> */}
       </ul>
     </nav>
   )    
