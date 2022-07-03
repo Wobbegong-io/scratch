329c6e612353ebcc624d0c51ac425e2f9e3b6b9d
EdnaK18 <reginakwanrx@gmail.com>
2022-07-03 10:44:46 -0400

    commiting to ensure any front end changes from collab session are available to pull

--- client/components/NavBar1.jsx
+++ client/components/NavBar1.jsx
@@ -1,25 +1,36 @@
 //this component will be the parent component for passing state
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignUp from './SignUp';
+import LogIn from './LogIn';
 
-export default function NavBar1() {
+export default function NavBar1({signedIn, setSignedIn}) {
 
   //add clickHandlers to render either log in or sign up 
-  // const LogIn = () => {
+  const LogIn = e => {
+    e.preventDefault();
+    return (
+      <LogIn signedIn={signedIn} setSignedIn={setSignedIn} />
+    )
+  }
 
-  // }
-
+  const signUp = e => {
+    e.preventDefault();
+    return(
+      <SignUp signedIn={signedIn} setSignedIn={setSignedIn}/> 
+     )
+  }
 
   return (
     <nav className='nav'>
       <Link to="/Landing" className='site-name'>Pick For Me</Link>
       <ul className='navbar'>
-        {/* <li>
-          <button onClick={()=>logIn}><p>Log In</p></button>
+        <li>
+          <button onClick={()=>logIn(e)}><p>Log In</p></button>
         </li>
         <li>
-          <button onClick={()=>signUp}><p>Sign Up</p></button>
-        </li> */}
+          <button onClick={()=>signUp(e)}><p>Sign Up</p></button>
+        </li>
       </ul>
     </nav>
   )    
